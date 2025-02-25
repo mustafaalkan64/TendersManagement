eeffb08f4e47b18320d4371269bd3c1abb2ca1f1
Mustafa Alkan <mustafa.alkan@eczacibasi.com.tr>
2025-02-25 11:27:32 +0300

    Units seed

---
+++ Offers/Migrations/20250225082340_AddMissingUnits.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Offers.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMissingUnits : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Units",
+                columns: new[] { "Id", "Name", "ShortCode" },
+                values: new object[,]
+                {
+                    { 1, "Kg", "Kg" },
+                    { 2, "Ton", "Ton" },
+                    { 3, "Lt", "Lt" },
+                    { 4, "Cm", "Cm" },
+                    { 5, "Mm", "Mm" },
+                    { 6, "M", "M" },
+                    { 7, "m³", "m³" },
+                    { 8, "m²", "m²" },
+                    { 9, "°", "°" },
+                    { 10, "Adet", "Adet" },
+                    { 11, "Hp", "Hp" },
+                    { 12, "-", "-" },
+                    { 13, "Evet", "Evet" },
+                    { 14, "Hayır", "Hayır" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Units",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Units",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Units",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Units",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Units",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Units",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Units",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Units",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Units",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Units",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Units",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Units",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Units",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Units",
+                keyColumn: "Id",
+                keyValue: 14);
+        }
+    }
+}
